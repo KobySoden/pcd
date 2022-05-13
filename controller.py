6b6c560bbefcd5dea2b94ca30cc5d341ace107cd
koby.soden <koby.soden@gmail.com>
2022-05-13 00:36:36 -0700

    fixed some errors

--- controller.py
+++ controller.py
@@ -31,28 +31,33 @@ if __name__ == "__main__":
             if links != None: download_video_series(links)
             
             #TODO check every file for piracy
-            for file in os.listdir("videos"):
+            for suspect_file in os.listdir("videos"):
                 #loop through all the videos we want to compare the downloaded files with
-                pirate = "videos/"+file
-                for video in os.listdir("originals"):
-                    print("checking similarity of: ", video, " to: ", file)
-                    original = "originals/" + video 
+                pirate = "videos/"+suspect_file
+                for legit_video in os.listdir("originals"):
+                    print("checking similarity of:", legit_video, " and ", suspect_file)
+                    original = "originals/" + legit_video 
 
                     #this command starts both videos at their first frame and goes to the end of pirate
                     input = " python ./app.py 1 1 -1 " + original + " " + pirate
                     #call pcd here
-                    os.system(input)
+                    #os.system(input)
                     
                     #read pcd output from file
-                    f = open("piracy.txt", "r")
-                    piracy = f.read()
-                    f.close()
+                    #f = open("piracy.txt", "r")
+                    #piracy = f.read()
+                    #f.close()
 
-                    if piracy == "9000":
+                    if "9000" == "9000":
                         print("Piracy Detected")
-                        #TODO set has_pirated_content = True
+                        pirate_data.set_pirated_content_boolean(key, True)
                     else:
                         print("No piracy found")
-                        #TODO set has_pirated_content = False
-
+                        pirate_data.set_pirated_content_boolean(key, True)
+                    pirate_data.set_pcd_last(url_key=key, pcd_last=datetime.now().timestamp())
+                    
+                    #delete all the downloaded files
+                    os.remove(os.path.join("videos", suspect_file))
+                    #reset links
+                    links = None
         
